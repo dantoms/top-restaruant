e3ae4b9d4bc987ab7933b3498a94f1a4d165c308
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-04-13 21:15:16 +0100

    add navigation functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+import { menu } from "./menu";
+import { home } from "./home";
+import { about } from "./about";
+
+(() => {
+  const nav = document.querySelector("nav");
+
+  const navClick = (e) => {
+    const content = document.querySelector("#content");
+    content.innerHTML = "";
+    if (e.target.textContent === "Menu") {
+      menu();
+    } else if (e.target.textContent === "Home") {
+      home();
+    } else if (e.target.textContent === "About") {
+      about();
+    }
+  };
+  nav.addEventListener("click", navClick);
+})();
